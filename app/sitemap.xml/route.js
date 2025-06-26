1ee5e85caeca07921d66b0b5a032d7eb0c58beca
drpvn18 <praveennaidu468@gmail.com>
2025-06-27 02:43:12 +0530

    added search bar for store/products

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -13,14 +13,22 @@ export async function GET() {
         'https://www.indianheritage.eu/refund-return-policy'
     ];
 
+    // const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    // <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    //   ${urls.map(url => `
+    //     <url>
+    //       <loc>${url}</loc>
+    //       <lastmod>${new Date().toISOString()}</lastmod>
+    //       <changefreq>monthly</changefreq>
+    //       <priority>1.0</priority>
+    //     </url>`).join('')}
+    // </urlset>`;
+
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${urls.map(url => `
         <url>
           <loc>${url}</loc>
-          <lastmod>${new Date().toISOString()}</lastmod>
-          <changefreq>monthly</changefreq>
-          <priority>1.0</priority>
         </url>`).join('')}
     </urlset>`;
 
