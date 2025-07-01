a3102aec8fb112a0cb966b25300a159f88516875
drpvn18 <praveennaidu468@gmail.com>
2025-07-01 21:02:17 +0530

    chnaged the sitemap

--- app/category/non-gi-products/[slug]/others/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function OtherProducts() {
-    return (
-        <div></div>
-    );
-}
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     siteUrl: 'https://www.indianheritage.eu',
     generateRobotsTxt: true,
+
+    exclude: [],
+
+    additionalPaths: async (config) => [
+        await config.transform(config, '/favicon.ico'),
+    ],
 };
