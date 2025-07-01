3a7232fd8bb69a2c9b56c3a7729a6086149fdaa1
drpvn18 <praveennaidu468@gmail.com>
2025-07-01 21:09:18 +0530

    video added to sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,5 +6,6 @@ module.exports = {
 
     additionalPaths: async (config) => [
         await config.transform(config, '/favicon.ico'),
+        await config.transform(config, '/videos/why_indian_heritage-1.mp4'),
     ],
 };
