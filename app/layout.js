5c8ee1ce0ab1b11f17c70f3f7beb9e6532861920
drpvn18 <praveennaidu468@gmail.com>
2025-07-01 23:54:50 +0530

    added google analytics

--- app/layout.js
+++ app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX', {
+              gtag('config', 'G-5LFRR61BGK', {
                 page_path: window.location.pathname,
               });
             `,
